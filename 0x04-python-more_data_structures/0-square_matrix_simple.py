14cd1f5556cc71c702c194b65a5634d1e84df69f
JEPTUI <alicejeptui@gmail.com>
2023-03-16 05:18:48 -0700

    Square values of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    #compute square values of all integers in a matrix
+    # compute square values of all integers in a matrix
     return ([list(map(lambda x: x * x, row)) for row in matrix])
