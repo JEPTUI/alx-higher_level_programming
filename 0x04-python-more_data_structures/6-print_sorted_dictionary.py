17d12bf8f13d3ef598dda1ac72ad0e8effe249e8
JEPTUI <alicejeptui@gmail.com>
2023-03-16 06:14:19 -0700

    Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    # print a dictionary by ordered keys
+    [print("{}: {}".format(x, a_dictionary[x])) for x in sorted(a_dictionary)]
