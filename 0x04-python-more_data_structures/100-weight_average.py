79adf9e8161ff191ce0a9f1b87cb2e679bc9f9bd
JEPTUI <alicejeptui@gmail.com>
2023-03-16 07:39:31 -0700

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+    total = 0
+    frequency = 0
+    for tup in my_list:
+        total += (tup[0] * tup[1])
+        frequency += tup[1]
+        return (total/frequency)
