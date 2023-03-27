31c605801645315cbe67782255e93d7b25684d89
JEPTUI <alicejeptui@gmail.com>
2023-03-27 03:54:56 -0700

    Second hot fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import stderr
+import sys
+
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except (ZeroDivisionError, IndexError, TypeError, ValueError) as e:
-        stderr.write("Exception: {}\n".format(e))
+    except (ZeroDivisionError, IndexError, TypeError, ValueError):
+        print("Exception: {}\n".format(sys.exc_info()[1]), file=sys.stderr)
         return None
