8418e58a7187e58f1613bea5c03987175c14a44a
JEPTUI <alicejeptui@gmail.com>
2023-07-25 04:11:58 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 const id = process.argv[2];
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/:id' + id;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + id;
 const request = require('request');
 
-request(apiUrl, (err, response, body) => {
+request(apiUrl, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body.title);
-  } else {
+  } else if (response.statusCode !== 200) {
     console.log('Error Code:' + response.statusCode);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
   }
 });
